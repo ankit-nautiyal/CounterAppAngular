75188b401130322dfa8e3bb22d39bb1a55085899
Ankit Nautiyal <nautiyalankit65@gmail.com>
2026-01-18 19:47:40 +0530

    CounterApp made

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,36 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('CounterApp');
+
+  count: number= 0;
+
+  // handleIncr(){
+  //   this.count= this.count+1;
+  // }
+
+  // handleDecr(){
+  //   if (this.count>0) {
+  //     this.count= this.count-1;
+  //   }
+  // }
+
+  // handleReset(){
+  //   this.count= 0;
+  // }
+
+  handleCounter(val: string){
+    if (val === '+') {
+      this.count= this.count+1;
+    } else if(val === '-' && this.count>0){
+      this.count= this.count-1;
+    } else{
+      this.count=0;
+    }
+  }
 }
